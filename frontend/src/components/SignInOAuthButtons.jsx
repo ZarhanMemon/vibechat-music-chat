6f38c158d0a815ff551022a40fc2dac22c1dcbaa
Zarhan Memon <ironmanred118@gmail.com>
2025-06-09 17:16:18 +0530

    Update SignInOAuthButtons.jsx

--- frontend/src/components/SignInOAuthButtons.jsx
+++ frontend/src/components/SignInOAuthButtons.jsx
@@ -17,19 +17,19 @@ const SignInOAuthButtons = () => {
     };
 
     return (
-        <Button
-            onClick={signInWithGoogle}
-            variant="secondary"
-            className="w-full h-11 text-white border-zinc-200 flex items-center justify-center gap-2 md:gap-3 text-sm md:text-base"
-        >
-            <img
-                src="/google.png"
-                alt="Google"
-                className="w-4 h-4 md:w-5 md:h-5"
-                loading="lazy"
-            />
-            <span className="truncate hidden md:block">Continue with Google</span>
-        </Button>
+     <Button
+  onClick={signInWithGoogle}
+  variant="secondary"
+  className="w-full h-11 text-white border-zinc-200 flex items-center justify-center gap-2 md:gap-3 text-sm md:text-base"
+>
+  <img
+    src="/google.png"
+    alt="Google"
+    className="w-4 h-4 md:w-5 md:h-5"
+    loading="lazy"
+  />
+  <span className="hidden md:block">Continue with Google</span>
+</Button>
     );
 };
 
