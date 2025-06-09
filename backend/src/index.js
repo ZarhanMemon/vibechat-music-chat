1b359821607dff166da3963465a0640a06972081
Zarhan Memon <ironmanred118@gmail.com>
2025-06-09 15:33:15 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -107,7 +107,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(clientBuildPath));
   // Catch-all must come **after** static & API routes
   app.get('/*', (_, res) => {
-    res.sendFile(path.join(clientBuildPath, '../frontend/dist/index.html'));
+    res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
 
